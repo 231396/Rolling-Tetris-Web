51f17b35b67f7ead0c8a3325212981059cac4180
vitor.marques <vitor.marques@gatec.com.br>
2020-11-03 22:00:30 -0300

    Php

---
+++ src/php/login.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <h1>Enviar dados para o BD (Login)</h1>
+        <?php
+            echo $_POST["login"];
+        ?>
+    </body>
+</html>
---
+++ src/php/signup.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <h1>Enviar dados para o BD (Signup)</h1>
+        <?php
+            printf ($_POST["name"]." - ");
+            printf ($_POST["datebirth"]." - ");
+            printf ($_POST["cpf"]." - ");
+            printf ($_POST["phone"]." - ");
+            printf ($_POST["email"]." - ");
+            printf ($_POST["username"]." - ");
+            printf ($_POST["gender"]."");
+        ?>
+    </body>
+</html>
