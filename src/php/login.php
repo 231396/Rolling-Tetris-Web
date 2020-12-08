f628da4b1ec10f3db6ce7264964135ce582eea1e
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2020-12-08 19:54:27 -0300

    php change

--- src/php/login.php
+++ src/php/login.php
@@ -1,8 +1,48 @@
-<html>
-    <body>
-        <h1>Enviar dados para o BD (Login)</h1>
-        <?php
-            echo $_POST["login"];
-        ?>
-    </body>
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+
+    <title>login</title>
+	<link rel="icon" type="image/ico" href="../../static/favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/style-login.css'>
+    <meta charset="utf-8" />
+    
+<body class="background">
+
+    <div class="box">
+        <h2 style="margin-bottom: 25px;">
+            <span style="color: #F30C36">T</span>
+            <span style="color: #FE8510">E</span>
+            <span style="color: #FFCC1E">T</span>
+            <span style="color: #4BC734">R</span>
+            <span style="color: #00B9E9">I</span>
+            <span style="color: #C01FBA">S</span>
+        </h2>    
+
+
+        <form class="formulario" id="form_login">
+
+            <label>Username</label>
+            <input class="input-login" type="text" name="login" maxlength="16" >
+            <span class="msg-erro msg-login"></span>
+            
+            <div style="margin-bottom: 10%;"></div>
+            
+            <label>Senha</label>
+            <input class="input-password" type="password" name="password"  maxlength="16" >
+            <span class="msg-erro msg-passw"></span>
+            
+            <div style="margin-bottom: 10%;"></div>
+            <div id="btn-holder">
+                <input class="btn" type="submit" value="Logar">
+                <a href="signup.html" class="btn">Cadastrar</a>
+            </div>
+        </form>
+    </div>
+    
+    <script src="../js/login.js"></script>
+</body>
+
 </html>
