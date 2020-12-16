fc5887a561d164c2fc3f9fb233659e82cd70f185
LucasSalu <samuel_6000@outlook.com>
2020-12-16 01:14:34 -0300

    campo-username

--- src/php/is_logged_in.php
+++ src/php/is_logged_in.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    $isLogged = isset($_SESSION['id'], $_SESSION['login'], $_SESSION['password']);
+    $isLogged = isset($_SESSION['id'], $_SESSION['username'], $_SESSION['password']);
 
     if (!$isLogged)
         echo '<div id="login-warn">Usario não logado <a href="../php_pages/login.php">(Realize o Login)</a></div>';
