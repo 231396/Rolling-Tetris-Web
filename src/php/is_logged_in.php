50cca013dbc410c82203d1959b5bf51e9a619729
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2020-12-23 11:41:12 -0300

    is_logged_in with database

--- src/php/is_logged_in.php
+++ src/php/is_logged_in.php
@@ -1,7 +1,26 @@
 <?php
     session_start();
-    $isLogged = isset($_SESSION['id'], $_SESSION['username'], $_SESSION['password']);
+	$isLogged = isset($_SESSION['id'], $_SESSION['username'], $_SESSION['password']);
 
+	if ($isLogged){
+		$isLogged = false;
+		try {
+			$conn = $_Database->new_PDO();
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$query = $conn->query("SELECT username, password, id FROM player where username = " . 
+			sql_string($_SESSION['username']) . "and password = " . sql_string($_SESSION['password']));
+			$credenciais = $query->fetch(PDO::FETCH_ASSOC);
+			if (isset($credenciais)) {
+				$_SESSION['id'] = $credenciais['id'];
+				$_SESSION['username'] = $credenciais['username'];
+				$_SESSION['password'] = $credenciais['password'];
+				$isLogged = true;
+			}
+		} catch(Exception $e) {
+			echo "Error: " . $e->getMessage();
+		}
+	}
+		
     if (!$isLogged)
         header('Location: ../php_pages/login.php');
 
