cbd97b12f9e3dec9f0cbd4cb554a9b9cd9332149
GislaineMarquess <37521373+GislaineMarquess@users.noreply.github.com>
2021-01-02 16:03:19 -0300

    Add files via upload

---
+++ src/php_pages/signup_success.php
@@ -0,0 +1,26 @@
+<?php
+    require "../php/database.php";
+?>
+
+<!DOCTYPE html>
+
+<html lang="pt">
+
+<head>
+    <title>signup</title>
+	<link rel="icon" type="image/ico" href="../../static/favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/style-signup.css'>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <form class="formulario" id="signup_form">
+        <h2>Fomulário de Cadastro</h2>
+        <label>Cadastro realizado com sucesso</label>
+        <p><a href="login.php">Fazer Login</a></p>
+    </form>
+
+</body>
+
+</html>
