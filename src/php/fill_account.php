b4861226f4459e3b3641d5de5bcff0c0fe75ddbf
AndreSacilotto <andre.sacilotto.dev@gmail.com>
2021-01-04 19:23:38 -0300

    Minor changes

--- src/php/fill_account.php
+++ src/php/fill_account.php
@@ -1,32 +1,26 @@
 <?php
-require "../php/database.php";
+	require "../php/database.php";
 
-$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
+	$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_SPECIAL_CHARS);
 
-session_start();
+	session_start();
 
-try {
-  $conn = $_Database->new_PDO();
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	try {
+	$conn = $_Database->new_PDO();
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $sql_query = "SELECT id, nome, dtNascimento, cpf, telefone, email, username, password FROM player where id = " . strval($id);
-  $query = $conn->query($sql_query);
+	$sql_query = "SELECT id, nome, dtNascimento, cpf, telefone, email, username, password FROM player where id = " . strval($id);
+	$query = $conn->query($sql_query);
 
-  $account = $query->fetch(PDO::FETCH_ASSOC);
-  //echo($sql_query);
-  //print_r($account);
+	$account = $query->fetch(PDO::FETCH_ASSOC);
+	//echo($sql_query);
+	//print_r($account);
 
-  echo json_encode($account);
+	echo json_encode($account);
 
-} catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-} catch (Exception $e) {
-  echo "Error: " . $e->getMessage();
-}
-
-function sql_string($str)
-{
-  return "'" . $str . "'";
-}
-
+	} catch (PDOException $e) {
+		echo "Connection failed: " . $e->getMessage();
+	} catch (Exception $e) {
+		echo "Error: " . $e->getMessage();
+	}
 ?>
--- src/php/verify_login.php
+++ src/php/verify_login.php
@@ -1,37 +1,33 @@
 <?php
-require "../php/database.php";
+	require "../php/database.php";
 
-$username = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
-$pass = filter_input(INPUT_POST, 'pass', FILTER_SANITIZE_SPECIAL_CHARS);
+	$username = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+	$pass = filter_input(INPUT_POST, 'pass', FILTER_SANITIZE_SPECIAL_CHARS);
 
-session_start();
+	session_start();
 
-try {
-  $conn = $_Database->new_PDO();
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $query = $conn->query("SELECT username, password, id FROM player where username = " . sql_string($username) . 'and password =  ' . sql_string($pass));
+	try {
+		$conn = $_Database->new_PDO();
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$query = $conn->query("SELECT username, password, id FROM player where username = '$username' 'and password = '$pass'");
 
-  $credenciais = $query->fetch(PDO::FETCH_ASSOC);
+		$credenciais = $query->fetch(PDO::FETCH_ASSOC);
 
-  if (isset($credenciais['id'], $credenciais['username'],  $credenciais['password'])) {
-    setcookie("id", $credenciais['id'], time() + 3600, "/");
-    setcookie("username", $credenciais['username'], time() + 3600, "/");
-    setcookie("password", $credenciais['password'], time() + 3600, "/");
-    $_SESSION['id'] = $credenciais['id'];
-    $_SESSION['username'] = $credenciais['username'];
-    $_SESSION['password'] = $credenciais['password'];
-  } else {
-    echo "false";
-  }
-} catch (PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-} catch (Exception $e) {
-  echo "Error: " . $e->getMessage();
-}
+		if (isset($credenciais['id'], $credenciais['username'],  $credenciais['password'])) {
+			$time = time() + 3600 * 8;
+			setcookie("id", $credenciais['id'], $time, "/");
+			setcookie("username", $credenciais['username'], $time, "/");
+			setcookie("password", $credenciais['password'], $time, "/");
+			$_SESSION['id'] = $credenciais['id'];
+			$_SESSION['username'] = $credenciais['username'];
+			$_SESSION['password'] = $credenciais['password'];
+		} else {
+			echo "false";
+		}
 
-function sql_string($str)
-{
-  return "'" . $str . "'";
-}
-
+	} catch (PDOException $e) {
+		echo "Connection failed: " . $e->getMessage();
+	} catch (Exception $e) {
+		echo "Error: " . $e->getMessage();
+	}
 ?>
